40ede396cc70b4f8a9a203c1c34b8508084e509c
Jishnu Nair <jishnair@Jishnus-MBP.fritz.box>
2020-05-11 16:58:22 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HireMe());
+}
+
+class HireMe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
